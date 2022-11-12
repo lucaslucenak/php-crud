8f2672fe0b3de079c6b0aff33d1573ce4d4d259f
Lucas Lucena <lucas.lucenak@gmail.com>
2022-11-12 20:17:35 -0300

    Update README.md

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title">Add a new student</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <p class="card-text">Here you can create a new student by clicking the button down.</p>
                         <a href="pages/create.php" class="btn btn-primary">Add Student</a>
                     </div>
                 </div>
@@ -14,7 +14,7 @@
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title">See all students</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <p class="card-text">Here you can take a look at the registered students and also edit or delete them.</p>
                         <a href="./pages/read.php" class="btn btn-primary">See Students</a>
                     </div>
                 </div>
