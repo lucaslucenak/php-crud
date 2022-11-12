8f6f87ecd582db9b8a6e662b0c627e9da570bbe1
Lucas Lucena <lucas.lucenak@gmail.com>
2022-11-12 01:12:42 -0300

    Update alertMessage location: Moved to read.php

--- pages/create.php
+++ pages/create.php
@@ -1,19 +1,7 @@
-<?php session_start(); ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-</head>
-<body>
+<?php include('../includes/header.php')?>
     <div class="container mt-5">
 
-        <?php
-        include('components/alertMessage.php')
-        ?>
+
 
         <div class="row">
             <div class="col-md-12">
@@ -63,6 +51,4 @@
             </div>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-</body>
-</html>
+<?php include('../includes/footer.php')?>
--- pages/read.php
+++ pages/read.php
@@ -1,18 +1,6 @@
-<?php session_start();
-require '../connection/dbConnection.php';
-?>
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-</head>
-<body>
+<?php include('../includes/header.php')?>
     <div class="row">
+        <?php include('components/alertMessage.php') ?>
         <div class="col-md-12">
             <div class="card-body">
                 <table class="table table-bordered table-striped">
@@ -29,6 +17,7 @@ require '../connection/dbConnection.php';
                     </thead>
                     <tbody>
                         <?php
+                            include ('../connection/dbConnection.php');
                             $connection = new Connection();
                             $query = "SELECT * FROM tb_student";
                             $query_run = mysqli_query($connection->con, $query);
@@ -65,6 +54,4 @@ require '../connection/dbConnection.php';
             <a href="../index.php" class="btn btn-danger float-end">BACK</a>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-</body>
-</html>
+<?php include('../includes/footer.php')?>
